117e9d7e4a5ae9dd4971e2b23286d48f0c8cdb8c
blackcode2 <crija19@gmail.com>
2025-02-21 16:02:13 -0500

    style main(card, button explore),api key, api configure

---
+++ js/api_configure.js
@@ -0,0 +1,64 @@
+"use strict";
+
+import API_KEY from '.env';
+import urlEncode from '../utils/urlEncode.js'
+
+const headers = new Headers();
+headers.append("Authorization", API_KEY);
+
+const requestOptions = { headers };
+
+const fetchData = async function (url, successCallback) {
+  const response = await fetch(url, requestOptions);
+
+  if (response.ok) {
+    const data = await response.json();
+    successCallback(data);
+  }
+}
+
+let requesUrl = "";
+
+const root = {
+  default: "https://api.pexels.com/v1/",
+  videos: "https://api.pexels.com/videos/"
+}
+
+export const client = {
+  photos: {
+    search(parameters, callback) {
+      requesUrl = `${root.default}search?${urlEncode(parameters)}`;
+      fetchData(requesUrl, callback);
+    },
+
+    curated(parameters, callback) {
+      fetchData(`${root.default}curated?${urlEncode(parameters)}`, callback);
+    },
+    detail(id, callback) {
+      fetchData(`${root.default}photos/${id}`, callback);
+    }
+  },
+  videos: {
+    search(parameters, callback) {
+      requesUrl = `${root.videos}search?${urlEncode(parameters)}`;
+      fetchData(requesUrl, callback);
+    },
+
+    popular(parameters, callback) {
+      fetchData(`${root.videos}popular?${urlEncode(parameters)}`, callback);
+    },
+    detail(id, callback) {
+      fetchData(`${root.videos}videos/${id}`, callback);
+    }
+  },
+  collections: {
+    featured(parameters, callback) {
+      requesUrl = `${root.default}collections/featured?${urlEncode(parameters)}`;
+      fetchData(requesUrl, callback);
+    },
+    detail(id, parameters, callback) {
+      requesUrl = `${root.default}collections/${id}?${urlEncode(parameters)}`;
+      fetchData(requesUrl, callback);
+    }
+  },
+}
--- js/global.js
+++ js/global.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import { addEventOnElements } from "../utils/handleEvent.js";
 import { ripple } from "../utils/ripple.js";
 
 //header 0n-scrolls
@@ -15,6 +16,17 @@ window.addEventListener("scroll", () => {
 const $rippleElems = document.querySelectorAll("[data-ripple]");
 
 $rippleElems.forEach($rippleElem => ripple($rippleElem));
+
+// navbar toggle for mobile screen
+
+const $navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const $navbar = document.querySelector("[data-navigation]");
+const $scrim = document.querySelector("[data-scrim]");
+
+addEventOnElements($navTogglers, "click", function () {
+  $navbar.classList.toggle("show");
+  $scrim.classList.toggle("active");
+})
 
 // filter functionality
 
