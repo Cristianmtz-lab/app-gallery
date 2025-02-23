a38f26d592505a2ea7248181ec84bbc1bc49596e
blackcode2 <crija19@gmail.com>
2025-02-22 19:45:24 -0500

    render videos in home page, cardBadge, style, created videoCard, hoverOnPlay

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 import { gridInit, updateGrid } from "../utils/masonry_grid.js";
 import { client } from "./api_configure.js";
 import { photoCard } from "./photo_card.js";
+import { videoCard } from "./video_card.js";
 
 // render curated photos in home page
 
@@ -15,8 +16,26 @@ client.photos.curated({ page: 1, pre_page: 15 }, data => {
   const photoGrid = gridInit($photoGrid);
 
   data.photos.forEach(photo => {
-    const $photocard = photoCard(photo);
+    const $photoCard = photoCard(photo);
 
-    updateGrid($photocard, photoGrid.columnsHeight, photoGrid.$columns);
+    updateGrid($photoCard, photoGrid.columnsHeight, photoGrid.$columns);
   })
 });
+
+// render popular videos in home page
+
+const $videoGrid = document.querySelector("[data-video-grid]");
+
+$videoGrid.innerHTML = `<div class="skeleton"></div>`.repeat(18);
+
+client.videos.popular({ pre_page: 20 }, data => {
+
+  $videoGrid.innerHTML = "";
+  const videoGrid = gridInit($videoGrid);
+
+  data.videos.forEach(video => {
+    const $videoCard = videoCard(video);
+
+    updateGrid($videoCard, videoGrid.columnsHeight, videoGrid.$columns);
+  })
+})
